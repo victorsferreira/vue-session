8dcffc8c64e63588c7f22213d2635ea0aaa2f320
Victor Ferreira <victor.ferreira@live.com>
2018-01-24 23:39:43 -0200

    Adding renew to other versions of the plugin

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ VueSession.install = function(Vue, options) {
             var all = this.getAll();
             all['session-id'] = 'sess:' + sessionId;
             VueSession.setAll(all);
-        }
+        },
         exists: function(){
             var all = this.getAll();
             return 'session-id' in all;
